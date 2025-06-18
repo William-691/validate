4bc99749692ecc1a82cade7328d1de7fbd8c9bba
SilvaX2024 <sinarjaya69111@gmail.com>
2025-06-18 15:26:55 +0700

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "validate",
-      script: "home/willdev/validate/server.js",
+      script: "./server.js",
       error_file: "/dev/null",
       out_file: "/dev/null",
     },
