eac25a51658ad3cfd56708392c76c0da97dce5c5
SilvaX2024 <sinarjaya69111@gmail.com>
2025-06-18 15:24:25 +0700

    test

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "validate",
+      script: "home/willdev/validate/server.js",
+      error_file: "/dev/null",
+      out_file: "/dev/null",
+    },
+  ],
+};
